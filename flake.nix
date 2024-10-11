01f0381b581d8fd2a37045834e81c85bedde0620
lewenhaupt <hugo.lewenhaupt@gmail.com>
2024-10-12 00:33:51 +0200

    Changing name again

--- flake.nix
+++ flake.nix
@@ -279,7 +279,7 @@
     packageDefinitions = {
       # These are the names of your packages
       # you can include as many as you wish.
-      lewenhaupt-nvim = { pkgs , ... }: {
+      dnvim = { pkgs , ... }: {
         # they contain a settings set defined above
         # see :help nixCats.flake.outputs.settings
         settings = {
@@ -326,7 +326,7 @@
     };
   # In this section, the main thing you will need to do is change the default package name
   # to the name of the packageDefinitions entry you wish to use as the default.
-    defaultPackageName = "lewenhaupt-nvim";
+    defaultPackageName = "dnvim";
   in
 
 
