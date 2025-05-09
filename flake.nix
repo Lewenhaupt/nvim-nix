42b67f061232ea3220379b2b9e091e307ea1e7b6
Hugo Lewenhaupt <hugo.lewenhaupt@gmail.com>
2025-05-10 00:35:36 +0200

    Adding vimtex

--- flake.nix
+++ flake.nix
@@ -207,6 +207,10 @@
               typescript
               nixfmt-rfc-style
             ];
+            latex = [
+              zathura
+              texliveFull
+            ];
             kickstart-debug = [ delve ];
             kickstart-lint = [
               markdownlint-cli
@@ -330,6 +334,9 @@
               #   ]
               # ))
             ];
+            latext = [
+              vimtex
+            ];
             kickstart-debug = [
               nvim-dap
               nvim-dap-ui
@@ -431,6 +438,58 @@
             # (and other information to pass to lua)
             categories = {
               general = true;
+              gitPlugins = true;
+              customPlugins = true;
+              test = true;
+
+              kickstart-autopairs = true;
+              kickstart-neo-tree = true;
+              kickstart-debug = true;
+              kickstart-lint = true;
+              kickstart-indent_line = true;
+
+              # this kickstart extra didnt require any extra plugins
+              # so it doesnt have a category above.
+              # but we can still send the info from nix to lua that we want it!
+              kickstart-gitsigns = true;
+
+              # we can pass whatever we want actually.
+              have_nerd_font = false;
+
+              example = {
+                youCan = "add more than just booleans";
+                toThisSet = [
+                  "and the contents of this categories set"
+                  "will be accessible to your lua with"
+                  "nixCats('path.to.value')"
+                  "see :help nixCats"
+                  "and type :NixCats to see the categories set in nvim"
+                ];
+              };
+            };
+          };
+        ltnvim =
+          { pkgs, ... }:
+          {
+            # they contain a settings set defined above
+            # see :help nixCats.flake.outputs.settings
+            settings = {
+              wrapRc = true;
+              configDirName = "nixCats-nvim";
+              # IMPORTANT:
+              # your alias may not conflict with your other packages.
+              aliases = [
+                "vim"
+                "vi"
+                "nvim"
+              ];
+              # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
+            };
+            # and a set of categories that you want
+            # (and other information to pass to lua)
+            categories = {
+              general = true;
+              latex = true;
               gitPlugins = true;
               customPlugins = true;
               test = true;
