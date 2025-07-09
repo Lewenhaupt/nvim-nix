07f83e1e98214bc85537a6468f5a84e069d07923
Hugo Lewenhaupt <hugo.lewenhaupt@gmail.com>
2025-07-09 21:42:10 +0200

    Removing invalid aliases

--- flake.nix
+++ flake.nix
@@ -553,11 +553,7 @@
               configDirName = "nixCats-nvim";
               # IMPORTANT:
               # your alias may not conflict with your other packages.
-              aliases = [
-                "vim"
-                "vi"
-                "nvim"
-              ];
+              aliases = [ ];
               # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
             };
             # and a set of categories that you want
