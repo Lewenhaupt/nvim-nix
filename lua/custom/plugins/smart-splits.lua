ad44be38894f7793b29e383f44cfc830b6ddba7a
Hugo Lewenhaupt <hugo.lewenhaupt@gmail.com>
2025-09-01 21:19:06 +0200

    Swap keybinds for smart-splits

--- lua/custom/plugins/smart-splits.lua
+++ lua/custom/plugins/smart-splits.lua
@@ -1,20 +1,21 @@
 return {
   'mrjones2014/smart-splits.nvim',
   lazy = false,
-  config = function()
-    vim.keymap.set('n', '<M-S-h>', require('smart-splits').resize_left)
-    vim.keymap.set('n', '<M-S-j>', require('smart-splits').resize_down)
-    vim.keymap.set('n', '<M-S-k>', require('smart-splits').resize_up)
-    vim.keymap.set('n', '<M-S-l>', require('smart-splits').resize_right)
-    -- moving between splits
-    vim.keymap.set('n', '<M-h>', require('smart-splits').move_cursor_left)
-    vim.keymap.set('n', '<M-j>', require('smart-splits').move_cursor_down)
-    vim.keymap.set('n', '<M-k>', require('smart-splits').move_cursor_up)
-    vim.keymap.set('n', '<M-l>', require('smart-splits').move_cursor_right)
-    -- swapping buffers between windows
-    vim.keymap.set('n', '<leader>sh', require('smart-splits').swap_buf_left)
-    vim.keymap.set('n', '<leader>sj', require('smart-splits').swap_buf_down)
-    vim.keymap.set('n', '<leader>sk', require('smart-splits').swap_buf_up)
-    vim.keymap.set('n', '<leader>sl', require('smart-splits').swap_buf_right)
-  end,
+  keys = {
+    -- Resize
+    { '<leader>srh', require('smart-splits').resize_left, mode = 'n', desc = 'Resize Left' },
+    { '<leader>srj', require('smart-splits').resize_down, mode = 'n', desc = 'Resize Down' },
+    { '<leader>srk', require('smart-splits').resize_up, mode = 'n', desc = 'Resize Up' },
+    { '<leader>srl', require('smart-splits').resize_right, mode = 'n', desc = 'Resize Right' },
+    -- Move
+    { '<leader>sh', require('smart-splits').move_cursor_left, mode = 'n', desc = 'Move Left' },
+    { '<leader>sj', require('smart-splits').move_cursor_down, mode = 'n', desc = 'Move Down' },
+    { '<leader>sk', require('smart-splits').move_cursor_up, mode = 'n', desc = 'Move Up' },
+    { '<leader>sl', require('smart-splits').move_cursor_right, mode = 'n', desc = 'Move Right' },
+    -- Swap
+    { '<leader>ssh', require('smart-splits').swap_buf_left, mode = 'n', desc = 'Swap Left' },
+    { '<leader>ssj', require('smart-splits').swap_buf_down, mode = 'n', desc = 'Swap Down' },
+    { '<leader>ssk', require('smart-splits').swap_buf_up, mode = 'n', desc = 'Swap Up' },
+    { '<leader>ssl', require('smart-splits').swap_buf_right, mode = 'n', desc = 'Swap Right' },
+  },
 }
