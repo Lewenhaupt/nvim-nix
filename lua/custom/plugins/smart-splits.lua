0fca74401cf721d1c6256a8b5e27a6b6ac079a0e
Hugo Lewenhaupt <hugo.lewenhaupt@gmail.com>
2025-09-02 21:18:06 +0200

    Fix issue with lazyvim keys and lazyloading

--- lua/custom/plugins/smart-splits.lua
+++ lua/custom/plugins/smart-splits.lua
@@ -3,19 +3,103 @@ return {
   lazy = false,
   keys = {
     -- Resize
-    { '<leader>srh', require('smart-splits').resize_left, mode = 'n', desc = 'Resize Left' },
-    { '<leader>srj', require('smart-splits').resize_down, mode = 'n', desc = 'Resize Down' },
-    { '<leader>srk', require('smart-splits').resize_up, mode = 'n', desc = 'Resize Up' },
-    { '<leader>srl', require('smart-splits').resize_right, mode = 'n', desc = 'Resize Right' },
+    {
+      '<leader>srh',
+      function()
+        require('smart-splits').resize_left()
+      end,
+      mode = 'n',
+      desc = 'Resize Left',
+    },
+    {
+      '<leader>srj',
+      function()
+        require('smart-splits').resize_down()
+      end,
+      mode = 'n',
+      desc = 'Resize Down',
+    },
+    {
+      '<leader>srk',
+      function()
+        require('smart-splits').resize_up()
+      end,
+      mode = 'n',
+      desc = 'Resize Up',
+    },
+    {
+      '<leader>srl',
+      function()
+        require('smart-splits').resize_right()
+      end,
+      mode = 'n',
+      desc = 'Resize Right',
+    },
     -- Move
-    { '<leader>sh', require('smart-splits').move_cursor_left, mode = 'n', desc = 'Move Left' },
-    { '<leader>sj', require('smart-splits').move_cursor_down, mode = 'n', desc = 'Move Down' },
-    { '<leader>sk', require('smart-splits').move_cursor_up, mode = 'n', desc = 'Move Up' },
-    { '<leader>sl', require('smart-splits').move_cursor_right, mode = 'n', desc = 'Move Right' },
+    {
+      '<leader>sh',
+      function()
+        require('smart-splits').move_cursor_left()
+      end,
+      mode = 'n',
+      desc = 'Move Left',
+    },
+    {
+      '<leader>sj',
+      function()
+        require('smart-splits').move_cursor_down()
+      end,
+      mode = 'n',
+      desc = 'Move Down',
+    },
+    {
+      '<leader>sk',
+      function()
+        require('smart-splits').move_cursor_up()
+      end,
+      mode = 'n',
+      desc = 'Move Up',
+    },
+    {
+      '<leader>sl',
+      function()
+        require('smart-splits').move_cursor_right()
+      end,
+      mode = 'n',
+      desc = 'Move Right',
+    },
     -- Swap
-    { '<leader>ssh', require('smart-splits').swap_buf_left, mode = 'n', desc = 'Swap Left' },
-    { '<leader>ssj', require('smart-splits').swap_buf_down, mode = 'n', desc = 'Swap Down' },
-    { '<leader>ssk', require('smart-splits').swap_buf_up, mode = 'n', desc = 'Swap Up' },
-    { '<leader>ssl', require('smart-splits').swap_buf_right, mode = 'n', desc = 'Swap Right' },
+    {
+      '<leader>ssh',
+      function()
+        require('smart-splits').swap_buf_left()
+      end,
+      mode = 'n',
+      desc = 'Swap Left',
+    },
+    {
+      '<leader>ssj',
+      function()
+        require('smart-splits').swap_buf_down()
+      end,
+      mode = 'n',
+      desc = 'Swap Down',
+    },
+    {
+      '<leader>ssk',
+      function()
+        require('smart-splits').swap_buf_up()
+      end,
+      mode = 'n',
+      desc = 'Swap Up',
+    },
+    {
+      '<leader>ssl',
+      function()
+        require('smart-splits').swap_buf_right()
+      end,
+      mode = 'n',
+      desc = 'Swap Right',
+    },
   },
 }
