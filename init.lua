b9ae0fe4e00449f5be81968e33b0b6f006ceb7d9
Hugo Lewenhaupt <hugo.lewenhaupt@gmail.com>
2025-09-03 21:50:12 +0200

    Disable some lsp servers for work

--- init.lua
+++ init.lua
@@ -572,9 +572,11 @@ require('nixCatsUtils.lazyCat').setup(pluginList, nixLazyPath, {
       -- NOTE: nixCats: there is help in nixCats for lsps at `:h nixCats.LSPs` and also `:h nixCats.luaUtils`
       local servers = {}
       -- servers.clangd = {},
-      servers.gopls = {}
-      servers.pyright = {}
-      servers.rust_analyzer = {}
+      if require('utils.init').nowork() then
+        servers.gopls = {}
+        servers.pyright = {}
+        servers.rust_analyzer = {}
+      end
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -587,7 +589,7 @@ require('nixCatsUtils.lazyCat').setup(pluginList, nixLazyPath, {
       -- NOTE: nixCats: nixd is not available on mason.
       if require('nixCatsUtils').isNixCats then
         servers.nixd = {}
-      else
+      elseif require('utils.init').nowork() then
         servers.rnix = {}
         servers.nil_ls = {}
       end
--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -4,4 +4,8 @@ M.nocode = function()
   return vim.fn.exists 'g:vscode' == 0
 end
 
+M.nowork = function()
+  return os.getenv 'IS_WORK' == 'Y'
+end
+
 return M
