8072a17f560cc394185bb8e60ec09ed63e1af501
Hugo Lewenhaupt <hugo.lewenhaupt@gmail.com>
2025-09-22 23:50:54 +0200

    Disable amazonq at work because it's broken for pro users not in default us region

--- lua/custom/plugins/amazonq.lua
+++ lua/custom/plugins/amazonq.lua
@@ -1,6 +1,6 @@
 return {
   'awslabs/amazonq.nvim',
-  enabled = require('nixCatsUtils').enableForCategory 'ai',
+  enabled = require('nixCatsUtils').enableForCategory 'ai' and require('utils.init').nowork(),
   opts = {
     ssoStartUrl = os.getenv 'AMAZON_Q_SSO_URL' or 'https://view.awsapps.com/start', -- Authenticate with Amazon Q Free Tier
   },
