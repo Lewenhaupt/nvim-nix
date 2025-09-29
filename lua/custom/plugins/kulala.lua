78cbd19c35d2c36032d472ee25868e03c33f111c
Hugo Lewenhaupt <hugo.lewenhaupt@gmail.com>
2025-09-29 18:09:30 +0200

    Change kulala prefix and add keymapping for selecting env

--- lua/custom/plugins/kulala.lua
+++ lua/custom/plugins/kulala.lua
@@ -1,10 +1,19 @@
+local kulala_prefix = '<leader>r'
 return {
   'mistweaverco/kulala.nvim',
-  keys = {},
   ft = { 'http', 'rest' },
   opts = {
     global_keymaps = true,
-    global_keymaps_prefix = '<leader>R',
+    global_keymaps_prefix = kulala_prefix,
     kulala_keymaps_prefix = '',
+  },
+  keys = {
+    {
+      kulala_prefix .. 'e',
+      function()
+        require('kulala').set_selected_env()
+      end,
+      desc = 'Select [E]nvironment',
+    },
   },
 }
