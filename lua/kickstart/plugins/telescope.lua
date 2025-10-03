ccfc5fb556b2e4291f9d039785f6ceeff42a219f
Hugo Lewenhaupt <hugo.lewenhaupt@gmail.com>
2025-10-03 22:06:45 +0200

    Don't ignore hidden files in no-ignore fG

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -111,7 +111,7 @@ return {
     vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
     vim.keymap.set('n', '<leader>fG', function()
-      builtin.live_grep { additional_args = { '-u' } }
+      builtin.live_grep { additional_args = { '-uu' } }
     end, { desc = '[F]ind by [G]rep (no ignore)' })
     vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
     vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
