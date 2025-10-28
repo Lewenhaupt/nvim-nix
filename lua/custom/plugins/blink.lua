7c9829b0aceb3cf3812e55a5c4f38b7870ae2d95
Hugo Lewenhaupt <hugo.lewenhaupt@gmail.com>
2025-10-28 21:27:02 +0100

    Change to full range for blink completion

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -33,7 +33,7 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    completion = { keyword = { range = 'full' }, documentation = { auto_show = false } },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
