9c2944c0f9b0f178479583b02e0f7a0ff0ee6517
lewenhaupt <hugo.lewenhaupt@gmail.com>
2025-11-17 21:54:18 +0100

    Moving keymaps to lsp config from snacks-picker

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -36,16 +36,81 @@ return {
         end,
         desc = 'Lsp Info',
       },
+      -- {
+      --   'gd',
+      --   vim.lsp.buf.definition,
+      --   desc = 'Goto Definition',
+      --   -- has = 'definition'
+      -- },
+      -- { 'gr', vim.lsp.buf.references, desc = 'References', nowait = true },
+      -- { 'gI', vim.lsp.buf.implementation, desc = 'Goto Implementation' },
+      -- { 'gy', vim.lsp.buf.type_definition, desc = 'Goto T[y]pe Definition' },
+      -- { 'gD', vim.lsp.buf.declaration, desc = 'Goto Declaration' },
       {
         'gd',
-        vim.lsp.buf.definition,
+        function()
+          Snacks.picker.lsp_definitions()
+        end,
         desc = 'Goto Definition',
-        -- has = 'definition'
       },
-      { 'gr', vim.lsp.buf.references, desc = 'References', nowait = true },
-      { 'gI', vim.lsp.buf.implementation, desc = 'Goto Implementation' },
-      { 'gy', vim.lsp.buf.type_definition, desc = 'Goto T[y]pe Definition' },
-      { 'gD', vim.lsp.buf.declaration, desc = 'Goto Declaration' },
+      {
+        'gD',
+        function()
+          Snacks.picker.lsp_declarations()
+        end,
+        desc = 'Goto Declaration',
+      },
+      {
+        'gr',
+        function()
+          Snacks.picker.lsp_references()
+        end,
+        nowait = true,
+        desc = 'References',
+      },
+      {
+        'gI',
+        function()
+          Snacks.picker.lsp_implementations()
+        end,
+        desc = 'Goto Implementation',
+      },
+      {
+        'gy',
+        function()
+          Snacks.picker.lsp_type_definitions()
+        end,
+        desc = 'Goto T[y]pe Definition',
+      },
+      {
+        'gai',
+        function()
+          Snacks.picker.lsp_incoming_calls()
+        end,
+        desc = 'C[a]lls Incoming',
+      },
+      {
+        'gao',
+        function()
+          Snacks.picker.lsp_outgoing_calls()
+        end,
+        desc = 'C[a]lls Outgoing',
+      },
+      {
+        '<leader>sS',
+        function()
+          Snacks.picker.lsp_symbols()
+        end,
+        desc = 'LSP Symbols',
+      },
+      {
+        '<leader>sW',
+        function()
+          Snacks.picker.lsp_workspace_symbols()
+        end,
+        desc = 'LSP Workspace Symbols',
+      },
+
       -- TODO: We currently set this in typescript.lua via better-type-hover, but ideally we should be able to set this one if that one doesn't exist
       -- {
       --   'K',
--- lua/custom/plugins/snacks-picker.lua
+++ lua/custom/plugins/snacks-picker.lua
@@ -411,7 +411,7 @@ return {
     {
       '<leader>fg',
       function()
-        Snacks.picker.git_grep() 
+        Snacks.picker.git_grep()
       end,
       desc = 'Grep',
     },
@@ -657,71 +657,6 @@ return {
         Snacks.picker.undo()
       end,
       desc = 'Undo History',
-    },
-    -- LSP
-    {
-      'gd',
-      function()
-        Snacks.picker.lsp_definitions()
-      end,
-      desc = 'Goto Definition',
-    },
-    {
-      'gD',
-      function()
-        Snacks.picker.lsp_declarations()
-      end,
-      desc = 'Goto Declaration',
-    },
-    {
-      'gr',
-      function()
-        Snacks.picker.lsp_references()
-      end,
-      nowait = true,
-      desc = 'References',
-    },
-    {
-      'gI',
-      function()
-        Snacks.picker.lsp_implementations()
-      end,
-      desc = 'Goto Implementation',
-    },
-    {
-      'gy',
-      function()
-        Snacks.picker.lsp_type_definitions()
-      end,
-      desc = 'Goto T[y]pe Definition',
-    },
-    {
-      'gai',
-      function()
-        Snacks.picker.lsp_incoming_calls()
-      end,
-      desc = 'C[a]lls Incoming',
-    },
-    {
-      'gao',
-      function()
-        Snacks.picker.lsp_outgoing_calls()
-      end,
-      desc = 'C[a]lls Outgoing',
-    },
-    {
-      '<leader>sS',
-      function()
-        Snacks.picker.lsp_symbols()
-      end,
-      desc = 'LSP Symbols',
-    },
-    {
-      '<leader>sW',
-      function()
-        Snacks.picker.lsp_workspace_symbols()
-      end,
-      desc = 'LSP Workspace Symbols',
     },
   },
 }
